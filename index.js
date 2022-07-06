e1578a1cb5dc582900e964c5b5e52937d7b1b21e
Amira de Moya <demoyaamira@gmail.com>
2022-07-05 22:12:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
-// Write your code here
+// multiply two nnumber that = 62
+
+let num1 = 31
+let num2 = 2
+
+const multiply = num1 * num2
+
+
+// combining math methods to get a random number greater than 0
+let random = Math.floor(Math.random() *10) + 1;
+ 
+// dividing two number that gives the answer of 4
+
+ let num3 = 16
+ let num4 = 4
+
+ const mod = num3 / num4
+
+
+//using Math.max to get the highest constituent in a set of number 
+
+ let max = Math.max(5, 10, 15, 20)
+
+ 
